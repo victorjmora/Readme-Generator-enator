fcca80f07dd888d49aa5a850c3b3d34b41f73bfc
victormora1 <victorjmora1@gmail.com>
2023-07-18 23:56:26 -0400

    added the licenses and the badges const

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     // Map license string to badge image or URL
@@ -13,7 +12,6 @@ function renderLicenseBadge(license) {
   }
   
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
     // Map license string to license URL
@@ -28,7 +26,6 @@ function renderLicenseLink(license) {
   }
   
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
     // Call renderLicenseBadge() and renderLicenseLink() functions to generate badge and link
@@ -42,7 +39,7 @@ function renderLicenseSection(license) {
   }
   
 
-// TODO: Create a function to generate markdown for README
+
 function generateMarkdown(data) {
     // Call renderLicenseSection() function to generate license section
     const licenseSection = renderLicenseSection(data.license);
